9c9f58c441edfac73241d5d4d06134a6cca69665
Control11 <controll11@gmail.com>
2024-06-16 20:24:06 +0200

    Change consumer topic name

--- producer/src/main/java/com/example/producer/ProducerController.java
+++ producer/src/main/java/com/example/producer/ProducerController.java
@@ -1,24 +1,25 @@
 package com.example.producer;
 
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/produce")
 public class ProducerController {
 
+    @NonNull
     private final KafkaTemplate<String, String> kafkaTemplate;
-
-    public ProducerController(KafkaTemplate<String, String> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
 
     @PostMapping("/{message}")
     public String sendMessage(@PathVariable String message) {
-        kafkaTemplate.send("price_detection", message);
+        kafkaTemplate.send("topic-name", message);
         return "Message sent to Kafka: " + message;
     }
+
 }
